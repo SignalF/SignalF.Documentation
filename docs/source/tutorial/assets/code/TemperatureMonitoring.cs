8eea3639a399cbf248b46478b63d2d0950507b52
Olaf Meyer <olaf.meyer@scotec-software.com>
2024-04-04 00:58:08 +0200

    Added image, code and text.

---
+++ docs/source/tutorial/assets/code/TemperatureMonitoring.cs
@@ -0,0 +1,89 @@
+﻿using SignalF.Controller;
+using SignalF.Controller.Signals;
+using SignalF.Controller.Signals.Calculators;
+using SignalF.Datamodel.Calculation;
+
+namespace Tutorial.Monitoring;
+
+public class TemperatureMonitoring : Calculator<ICalculatorConfiguration>
+{
+    private const double WarnLevel = 70.0;
+    private const double CriticalLevel = 100.0;
+
+    private int _indexTemperature;
+    private int _indexOk;
+    private int _indexWarning;
+    private int _indexAlarm;
+    private int _indexFan;
+
+    public TemperatureMonitoring(ISignalHub signalHub, ILogger<TemperatureMonitoring> logger)
+        : base(signalHub, logger)
+    {
+    }
+
+    protected override void OnCalculate()
+    {
+        var timestamp = SignalHub.GetTimestamp();
+        var temperature = SignalSinks[_indexTemperature].Value;
+
+        var signalSources = SignalSources;
+        signalSources[_indexOk].AssignWith(temperature < WarnLevel ? 1.0 : 0.0, timestamp);
+        signalSources[_indexWarning].AssignWith(temperature is >= WarnLevel and < CriticalLevel ? 1.0 : 0.0, timestamp);
+        signalSources[_indexAlarm].AssignWith(temperature >= CriticalLevel ? 1.0 : 0.0, timestamp);
+        signalSources[_indexFan].AssignWith(temperature >= WarnLevel ? 1.0 : 0.0, timestamp);
+    }
+
+    protected override void OnConfigure(ICalculatorConfiguration configuration)
+    {
+        base.OnConfigure(configuration);
+
+        foreach (var signalSink in configuration.SignalSinks)
+        {
+            var signalName = signalSink.Definition.Name;
+            switch (signalName)
+            {
+                case "CpuTemperature":
+                {
+                    _indexTemperature = GetSignalIndex(signalSink);
+                    break;
+                }
+                default:
+                {
+                    throw new ControllerException($"Unknown signal {signalName}");
+                }
+            }
+        }
+
+        foreach (var signalSource in configuration.SignalSources)
+        {
+            var signalName = signalSource.Definition.Name;
+            switch (signalName)
+            {
+                case "OK":
+                {
+                    _indexOk = GetSignalIndex(signalSource);
+                    break;
+                }
+                case "Warning":
+                {
+                    _indexWarning = GetSignalIndex(signalSource);
+                    break;
+                }
+                case "Alarm":
+                {
+                    _indexAlarm = GetSignalIndex(signalSource);
+                    break;
+                }
+                case "Fan":
+                {
+                    _indexFan = GetSignalIndex(signalSource);
+                    break;
+                }
+                default:
+                {
+                    throw new ControllerException($"Unknown signal {signalName}");
+                }
+            }
+        }
+    }
+}
--- docs/source/tutorial/calculators.md
+++ docs/source/tutorial/calculators.md
@@ -5,6 +5,20 @@ Calculators have no direct connection to the hardware, but communicate exclusive
 The calculators are called up in special calculator tasks. A calculator task first calls the calculator assigned to it in  `read` mode. This gives the calculator the opportunity to read signals from the signal hub. Once the signals have been read, the calculator is called up in `calculate` mode. In the last step, the calculator is called up in `write` mode and can now write the calculated values back to the signal hub.
 
 ## Temperature monitoring
+In this part of the tutorial, we will add the temperature monitoring to our application in the form of a calculator.
+
+![Calculator](assets/images/Calculator.png)
+
+The temperature monitoring requires the current CPU temperature as an input signal. Depending on the temperature, we set the status to `OK`, `Warning` or `Alarm`. If the system is in `Warning` or `Alarm` status, the fan should also be switched on. 
+
+Create a new class `TemperatureMonitoring`.
+
+```{literalinclude} assets/code/TemperatureMonitoring.cs
+---
+language: csharp
+lines: 1-9,19-22,88-
+---
+```
 
 
 ## Configuration
